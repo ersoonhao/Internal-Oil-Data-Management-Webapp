540c0099bca47d8862937551eae3bb24232d1db6
ersoonhao <soonhao.er@gmail.com>
2021-11-13 18:06:06 +0800

    Update ProjectApplication.java

--- src/main/java/com/oop/project/ProjectApplication.java
+++ src/main/java/com/oop/project/ProjectApplication.java
@@ -13,7 +13,7 @@ public class ProjectApplication {
 	
 	public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ProjectApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
+        app.setDefaultProperties(Collections.singletonMap("server.port", "5000"));
         app.run(args);
 		// Webscrape.monthlyTask();
 	}
