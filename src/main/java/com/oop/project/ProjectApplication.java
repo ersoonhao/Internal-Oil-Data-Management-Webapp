da77b1ea85e9c75b5393b67c888181eaa52d5916
ersoonhao <soonhao.er@gmail.com>
2021-11-13 21:48:22 +0800

    Update ProjectApplication.java

--- src/main/java/com/oop/project/ProjectApplication.java
+++ src/main/java/com/oop/project/ProjectApplication.java
@@ -6,10 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.Collections;
+// added 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
 @EnableScheduling
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ProjectApplication.class);
